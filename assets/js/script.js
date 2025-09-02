56fa674976a4e91503153936d781d43466279b1d
clarkey164 <s.clarke0896@gmail.com>
2025-09-02 14:12:46 +0100

    js structure added

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateCorrectAnswer () {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}   
+
+function displayAdditionQuestion (operand1, operand2) {
+
+}   
+
+function displaySubtractQuestion (operand1, operand2) {
+
+}   
+
+function displayMultiplyQuestion (operand1, operand2) {
+
+}
+
+function displayDivisionQuestion (operand1, operand2) {
+
+}
