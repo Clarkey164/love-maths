ea7cdfd98acd8cf76e3df5ae54b9234d1400c3ca
clarkey164 <s.clarke0896@gmail.com>
2025-09-02 21:15:13 +0100

    Add if statement to ensure no negative numbers in subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,6 +119,15 @@ function displaySubtractQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "-";
+    
+    if (operand1 < operand2) {
+        // Swap the numbers to avoid negative answers
+        let temp = operand1;
+        operand1 = operand2;
+        operand2 = temp;
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
+    }
 }   
 
 function displayMultiplyQuestion(operand1, operand2) {
